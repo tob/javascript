9f621caad02b8554177db75d91c0a5efc38c3224
tob <tobiadonati@gmail.com>
2017-07-12 14:37:30 +0200

    Bind done toggle to checkbox onchange event

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js
+
+"use strict";
+var pi, pet, age, a, b, total, greeting, hour
+pi = 3.14;
+console.log(pi);
+
+pet = 'Hamster';
+console.log(pet);
+
+age = '12';
+console.log('My ' + pet + ' is ' + age + ' years old' );
+console.log('My', pet, 'is', age, 'years old' );
+
+a = 6
+b = 12
+
+total = a + b
+console.log(total);
+
+hour = 1
+greeting = (hour < 18) ? "Good day" : "Good evening"
+
+hour = 12
+greeting = (hour < 18) ? "Good day" : "Good evening"
+console.log(greeting)
+console.log(hour)
+
+hour = 19
+greeting = (hour < 18) ? "Good day" : "Good evening"
+console.log(greeting)
+console.log(hour)
